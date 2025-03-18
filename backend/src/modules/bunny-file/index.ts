d4d2f29f1f4019cec2474e6355fd8e077732a27d
yashaba <yashabazovner@gmail.com>
2025-03-18 22:59:44 +0200

    altered the bunny more

--- backend/src/modules/bunny-file/index.ts
+++ backend/src/modules/bunny-file/index.ts
@@ -1,10 +1,10 @@
-import { ModuleProviderExports } from '@medusajs/framework/types';
-import BunnyFileProviderService from './service';
+import { ModuleProviderExports } from "@medusajs/framework/types"
+import BunnyFileProviderService from "./service"
 
-const services = [BunnyFileProviderService];
+const services = [BunnyFileProviderService]
 
 const providerExport: ModuleProviderExports = {
   services,
-};
+}
 
-export default providerExport;
+export default providerExport
