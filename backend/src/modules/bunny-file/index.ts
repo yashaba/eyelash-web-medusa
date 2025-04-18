10dc2e692f5226e5709ef06559b952441685c280
yashaba <yashabazovner@gmail.com>
2025-04-19 01:05:22 +0300

    fix

--- backend/src/modules/bunny-file/index.ts
+++ backend/src/modules/bunny-file/index.ts
@@ -1,51 +1,10 @@
-// import axios from "axios";
-// import type { Request } from "express";
-// import type multer from "multer";
+import { ModuleProviderExports } from '@medusajs/framework/types';
+import BunnyFileProviderService from './service';
 
-// class BunnyFileService {
-//   protected readonly storageZoneName: string;
-//   protected readonly accessKey: string;
-//   protected readonly pullZoneUrl: string;
+const services = [BunnyFileProviderService];
 
-//   constructor({}, options) {
-//     this.storageZoneName = options.storageZoneName;
-//     this.accessKey = options.accessKey;
-//     this.pullZoneUrl = options.pullZoneUrl;
-//   }
+const providerExport: ModuleProviderExports = {
+  services,
+};
 
-//   async upload(file: multer.File): Promise<{ url: string }> {
-//     const fileName = `${Date.now()}-${file.originalname}`;
-//     const uploadUrl = `https://storage.bunnycdn.com/${this.storageZoneName}/${fileName}`;
-
-//     try {
-//       await axios.put(uploadUrl, file.buffer, {
-//         headers: {
-//           AccessKey: this.accessKey,
-//           "Content-Type": file.mimetype,
-//         },
-//       });
-
-//       return { url: `${this.pullZoneUrl}/${fileName}` };
-//     } catch (error) {
-//       console.error("BunnyCDN Upload Error:", error);
-//       throw new Error("Failed to upload file to BunnyCDN");
-//     }
-//   }
-
-//   async delete(fileKey: string): Promise<void> {
-//     const deleteUrl = `https://storage.bunnycdn.com/${this.storageZoneName}/${fileKey}`;
-
-//     try {
-//       await axios.delete(deleteUrl, {
-//         headers: {
-//           AccessKey: this.accessKey,
-//         },
-//       });
-//     } catch (error) {
-//       console.error("BunnyCDN Delete Error:", error);
-//       throw new Error("Failed to delete file from BunnyCDN");
-//     }
-//   }
-// }
-
-// export default BunnyFileService;
+export default providerExport;
